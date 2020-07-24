fb338535517052e2346ec63815a8c31d7bf1c349
zcc <934947091@qq.com>
2020-07-24 15:01:10 +0800

    zcc

--- app/src/main/java/com/example/zccproject/SecondActivity.java
+++ app/src/main/java/com/example/zccproject/SecondActivity.java
@@ -10,5 +10,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        //ceshi
     }
 }
