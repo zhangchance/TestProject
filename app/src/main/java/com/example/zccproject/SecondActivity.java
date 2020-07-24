b170bfddfeaeb44a377bc5b65b01e4a38c3b5114
zcc <934947091@qq.com>
2020-07-24 15:18:07 +0800

    zcc

--- app/src/main/java/com/example/zccproject/SecondActivity.java
+++ app/src/main/java/com/example/zccproject/SecondActivity.java
@@ -8,6 +8,7 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //123444s
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
